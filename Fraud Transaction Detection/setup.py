19b2e8bb5d1694cd33d683d381fae09c3abede2b
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2023-04-20 02:24:35 +0530

    setup

---
+++ Fraud Transaction Detection/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Fraud Transaction Detection System",
+    version="0.0.1",
+    description="Fraud Transaction Detection Systemt",
+    author="vaasu2002",
+    packages=find_packages(),
+    license="MIT"
+)
